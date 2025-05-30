bb7cfe05eb8af450545cf8e737480b4500acc41c
saikrishnavadali05 <saikrishnavadali05@gmail.com>
2025-05-30 18:22:54 +0530

    website accessible in mobile mode now

--- docs/New_Radio/generate_pages.py
+++ docs/New_Radio/generate_pages.py
@@ -15,7 +15,8 @@ def generate_html(title, relative_path_to_home="../../New-Radio-index.html"):
     return f"""<!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>{title}</title>
   <link rel="stylesheet" href="../../css/styles.css">
   <script defer src="../../js/script.js"></script>
